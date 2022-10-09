1587b4e8c96c4e6fb5e0d0b118970e6181cc7a06
Yeongjin Kwon <yeongjinkwon@gmail.com>
2022-10-09 14:38:52 -0400

    Reorganize repository structure
    Move all usb boot stuff into its own subdirectory.
    Also edit appropriate files to account for the moving.

--- file_install_mapping.bash
+++ file_install_mapping.bash
@@ -3,14 +3,14 @@
 # It uses the functions defined in the install script to do this.
 # The current directory is the directory of the install script.
 
-install_file -d update_usb_boot /usr/local/bin/
-install_file -d kexec_into_real_kernel /etc/usb-boot/
-install_to_directory mkinitcpio_hooks/* /etc/initcpio/install/
-install_file -d usb-boot.preset /etc/mkinitcpio.d/
-install_file -f mkinitcpio_image_filters/add_microcode \
+install_file -d usb_boot/update_usb_boot /usr/local/bin/
+install_file -d usb_boot/kexec_into_real_kernel /etc/usb-boot/
+install_to_directory usb_boot/mkinitcpio_hooks/* /etc/initcpio/install/
+install_file -d usb_boot/usb-boot.preset /etc/mkinitcpio.d/
+install_file -f usb_boot/mkinitcpio_image_filters/add_microcode \
     /usr/local/bin/embed_microcode_into_initramfs
 
-install_file -d reboot_with_kexec /usr/local/bin/
+install_file -d usb_boot/reboot_with_kexec /usr/local/bin/
 
 install_file -f rust_build/release/usbbootmgr /usr/local/bin/usbbootmgr
 
